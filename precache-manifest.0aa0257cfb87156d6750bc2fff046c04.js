5ce68a5dbc1a36df7ac73aaaa19b7bfba6655254
Leek <tuan@shopstack.asia>
2019-02-21 09:13:10 +0700

    ShopstackTraining - Upload exercise 01

---
+++ precache-manifest.0aa0257cfb87156d6750bc2fff046c04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5a341e2ecbf9a11367c",
+    "url": "/static/css/main.16d1f230.chunk.css"
+  },
+  {
+    "revision": "e5a341e2ecbf9a11367c",
+    "url": "/static/js/main.e5a341e2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8071778a30eafb2a39577c13760f3826",
+    "url": "/index.html"
+  }
+];
